32dc2b8db0c577fa7e54172f2a8bc852fbe6eaa1
isaac-miles <isaacnice247@gmail.com>
2024-08-15 09:13:51 +0100

    .

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   /* 
-    GET /users
-    Get /users/:id
+ 
+   
     POST /users
     PATCH /users/:id
     DELETE /Users/:id
  */
+  @Get() //GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get(':id') // Get /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
 }
